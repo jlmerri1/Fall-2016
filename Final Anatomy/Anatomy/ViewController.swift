2ab46a0cd0eafb48401c4e96d12cd1809c7e8186
Jason M <jlmerri1@neiu.edu>
2017-03-01 07:27:21 -0600

    Add anatomy quiz files via upload
    Final iOS project for mobile app class

---
+++ Final Anatomy/Anatomy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Anatomy
+//
+//  Created by Jason Merrill on 12/14/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Final Anatomy/Anatomy/armsThree.swift
@@ -0,0 +1,49 @@
+//
+//  armsThree.swift
+//  Anatomy
+//
+//  Created by Jason Merrill on 12/14/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+class armsThreeViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func wrongAnswer(sender:AnyObject)
+    {
+        let alertController = UIAlertController(title: "Wrong!", message: "Please try again!", preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { action in
+            // ...
+        }
+        alertController.addAction(OKAction)
+        
+        self.present(alertController, animated: true) {
+            // ...
+        }
+        
+    }
+    
+    
+    @IBAction func rightAnswer(sender:AnyObject){
+        
+        let alertController = UIAlertController(title: "HOORAY \n\n\n\n You are correct!!", message: "Good job!", preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "Next", style: .default) { action in
+            // ...
+            self.performSegue(withIdentifier: "home", sender: self)
+        }
+        alertController.addAction(OKAction)
+        
+        self.present(alertController, animated: true) {
+            // ...
+        }
+    }
+    
+}
+
